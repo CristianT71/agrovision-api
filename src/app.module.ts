089e594cbe5df9eef281ee79cd1261f632e77e33
Cristian Trujillo <cristiandanilotrujillo123@gmail.com>
2026-09-11 16:19:49 -0500

    fix: usar DB_USER en TypeOrm config

--- src/app.module.ts
+++ src/app.module.ts
@@ -10,7 +10,7 @@ import { SolicitudesModule } from "./modules/solicitudes/solicitudes.module";
             type: "postgres",
             host: process.env.DB_HOST,
             port: parseInt(process.env.DB_PORT ?? "5432"),
-            username: process.env.DB_USERNAME,
+            username: process.env.DB_USER,
             password: process.env.DB_PASSWORD,
             database: process.env.DB_NAME,
             autoLoadEntities: true,
