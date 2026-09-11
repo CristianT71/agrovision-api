7f810c5f1667492fba3b4741384af515e502fa45
Cristian Trujillo <cristiandanilotrujillo123@gmail.com>
2026-09-11 18:53:30 -0500

    chore(cors): habilitar CORS para desarrollo (http://localhost:5173)

--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,8 @@ async function bootstrap() {
     const app = await NestFactory.create(AppModule);
     app.setGlobalPrefix("api");
 
+    app.enableCors({ origin: "http://localhost:5173" });
+
     app.useGlobalPipes(
         new ValidationPipe({
             whitelist: true,
