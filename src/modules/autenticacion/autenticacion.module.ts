4ea1da1b2afc5e97bd959d77e9efffd506bf6bc9
Cristian Trujillo <cristiandanilotrujillo123@gmail.com>
2026-09-21 07:50:27 -0500

    fix(otp): solicitar otp mediante la terminal en pruebas

--- src/modules/autenticacion/autenticacion.module.ts
+++ src/modules/autenticacion/autenticacion.module.ts
@@ -9,9 +9,9 @@ import { TypeOrmSesionOtpEntity } from "./infrastructure/adapters/out/persistenc
 import { TypeOrmUsuarioRepository } from "./infrastructure/adapters/out/persistence/typeorm-usuario.repository";
 import { TypeOrmSesionOtpRepository } from "./infrastructure/adapters/out/persistence/typeorm-sesion-otp.repository";
 import { ZavuSmsAdapter } from "./infrastructure/adapters/out/sms/zavu-sms.adapter";
+import { LoggerSmsAdapter } from "./infrastructure/adapters/out/sms/logger-sms.adapter";
 import { AuthController } from "./infrastructure/adapters/in/http/auth.controller";
 import { JwtStrategy } from "./infrastructure/adapters/in/http/strategies/jwt.strategy";
-
 import { USUARIO_REPOSITORY } from "./domain/ports/out/usuario.repository";
 import { SESION_OTP_REPOSITORY } from "./domain/ports/out/sesion-otp.repository";
 import { SMS_SERVICE } from "./domain/ports/out/sms.service";
@@ -48,7 +48,7 @@ import { ValidarOtpService } from "./application/use-cases/validar-otp.service";
         },
         {
             provide: SMS_SERVICE,
-            useClass: ZavuSmsAdapter,
+            useClass: LoggerSmsAdapter,
         },
     ],
     exports: [SolicitarOtpService, ValidarOtpService, JwtModule, PassportModule],
